8769984006ba9259092e51a3eaeea191eff0052e
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-08-23 15:17:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+function findMatching(array, string){
+  return array.filter(function (name) {
+    return name.toLowerCase() === string.toLowerCase();
+  });
+};
+function fuzzyMatch(array, string){
+  let lengthOfLetters = string.length;
+  return array.filter(function (name){
+    return name.slice(0,lengthOfLetters) === string;
+  });
+};
+function matchName(array, string){
+  return array.filter(function(Obj){
+    return Obj.name.toLowerCase() === string.toLowerCase();
+  });
+};
