6cfe4cf50058ed16169f72f3ed6eae39e1367379
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-08-23 15:04:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(array, string){
-  return array.filter(function (string) {
-    return string = string;
+  return array.filter(function (name) {
+    return name.toLowerCase() === string.toLowerCase();
   });
 };
